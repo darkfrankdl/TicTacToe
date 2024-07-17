143a349abb598cc4148512fd6345427293f0adbe
darkfrankdl <businesslundt@gmail.com>
2024-07-17 19:04:46 +0200

    set up of aggregate roots, entities and value objects in Domain

--- TicTacToe/Domain/Entity/Piece.cs
+++ TicTacToe/Domain/Entity/Piece.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain
+namespace Domain.Entity
 {
-    public class Player
+    internal class Piece
     {
     }
 }
--- TicTacToe/Domain/ValueObject/Player2.cs
+++ TicTacToe/Domain/ValueObject/Player2.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain
+namespace Domain.ValueObject
 {
-    public class Game
+    internal class Player2
     {
     }
 }
