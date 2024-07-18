2f5220584eba2acbb6204bfe87bc6cb53aee8a79
darkfrankdl <businesslundt@gmail.com>
2024-07-18 14:26:14 +0200

    made it possible to draw the board visually

--- TicTacToe/TUI/Program.cs
+++ TicTacToe/TUI/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace program
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            drawBoard(new string[] { "X", "X", "O", "X", "X", "O", "X", "X", "O" });
+
+        }
+
+        private static void drawBoard (string[] boardStatus)
+        {
+
+            Console.WriteLine("-------");
+
+            Console.WriteLine($"|{boardStatus[0]}|{boardStatus[1]}|{boardStatus[2]}|");
+
+            Console.WriteLine($"|{boardStatus[3]}|{boardStatus[4]}|{boardStatus[5]}|");
+
+            Console.WriteLine($"|{boardStatus[6]}|{boardStatus[7]}|{boardStatus[8]}|");
+        }
+    }
+}
